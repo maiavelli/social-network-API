53112606e7ea6b867ce231a28ccf974c2925d7c3
Maia Davis <maia@Maias-Air.lan>
2022-08-09 16:17:52 -0500

    user model written

---
+++ models/User.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require('mongoose');
+
+//schema
+const userSchema = new Schema(
+    {
+        username: { 
+            type: String, 
+            unique: true, 
+            required: true, 
+            trim: true
+        },
+        email: { 
+            type: String, 
+            unique: true, 
+            required: true,
+            match: [
+                /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
+                'Please enter a valid email address!'
+            ],
+        },
+        thoughts: [ 
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            },
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        }, 
+        id: false,
+    }
+);
+
+userSchema.virtual('friendCount').get(function () {
+    return `${this.friends.length}`;
+});
+
+const User = model('user', userSchema);
+
+module.exports = User;
