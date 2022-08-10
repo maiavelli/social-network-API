3c9610bdb9de183490c7acf128d816b89e4be3db
Maia Davis <maia@Maias-Air.lan>
2022-08-09 20:08:45 -0500

    user routes written

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,7 +1,22 @@
 const router = require('express').Router();
 
 const {
-    getAllUsers,
-    getUserById,
+    getUsers,
+    getSingleUser,
     createUser,
-} = require('../../controllers.userControllers.js');
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend
+} = require('../../controllers.userController.js');
+
+// /api/users
+router.route('/').get(getUsers).post(createUser);
+
+// /api/users/:userId
+router.route('/:id').get(getSingleUser).put(updateUser).delete(deleteUser);
+
+// /api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId').post(addFriend).delete(deleteFriend);
+
+module.exports = router;
